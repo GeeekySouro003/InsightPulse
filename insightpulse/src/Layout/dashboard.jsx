db8a94c48f5e1dff4ddf25518e00380da5b9aa0d
Souradip <dgsouro77@gmail.com>
2024-06-22 22:35:37 +0530

    Updated the codebase

---
+++ insightpulse/src/Layout/dashboard.jsx
@@ -0,0 +1,18 @@
+
+import AdminDashboard from "@/components/AdminDashboard"
+import Navbarpanel from "@/components/Navbarpanel"
+import { Settings } from "@/components/Settings"
+import Sidebar from "@/components/Sidebar"
+
+
+
+const DashboardLayout = () => {
+  return (
+   <main>
+  
+    
+   </main>
+  )
+}
+
+export default DashboardLayout
