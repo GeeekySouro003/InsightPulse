794c990970715bdaf543d36888b5ebdd7f996671
Souradip <dgsouro77@gmail.com>
2024-06-23 00:28:25 +0530

    Added the auth route

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const fromRoute = require("./routes/formRoute");
+const authRoutes = require("./routes/authRoutes");
+const updatePasswordRoute = require("./routes/updatePasswordRoute");
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+
+app.use(cors());
+app.use(express.json());
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(()=>console.log("Connected to MongoDB"))
+    .catch(err=>console.error("Error connecting to MongoDB",err));
+
+app.use('/api', fromRoute);
+app.use("/auth", authRoutes); 
+app.use("/admin", updatePasswordRoute); 
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port: ${PORT}`);
+});
