64e431dcbf41f6f0b445e4ae856031c67469ae8f
Souradip <dgsouro77@gmail.com>
2024-06-23 11:55:23 +0530

    Backend server done

--- server/index.js
+++ server/index.js
@@ -1,25 +1,40 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const fromRoute = require("./routes/formRoute");
-const authRoutes = require("./routes/authRoutes");
-const updatePasswordRoute = require("./routes/updatePasswordRoute");
+// Import required modules and files
+const express = require('express');
+const connectDB = require("./config/db");
+const dotenv = require("dotenv");
+const cors = require('cors')
+const userRoutes = require("./routes/userRoutes");
+const feedbackRoutes = require("./routes/feeback");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Connect to the database
+connectDB();
+
+// Initialize Express app
 const app = express();
-const PORT = process.env.PORT || 8000;
-require('dotenv').config();
 
+// Using Cors
+app.use(cors())
 
-app.use(cors());
+// Middleware to parse JSON data
 app.use(express.json());
-mongoose.connect(process.env.MONGODB_URI)
-    .then(()=>console.log("Connected to MongoDB"))
-    .catch(err=>console.error("Error connecting to MongoDB",err));
 
-app.use('/api', fromRoute);
-app.use("/auth", authRoutes); 
-app.use("/admin", updatePasswordRoute); 
+// Define the port to listen on
+const port = process.env.PORT || 8001;
 
+// Mount user routes under /api/user path
+app.use("/api/user", userRoutes);
+app.use("/api/feeback", feedbackRoutes);
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port: ${PORT}`);
-});
+// Error handling middleware
+app.use(notFound); // Handle 404 errors
+app.use(errorHandler); // Handle other errors
+
+// Start the server
+app.listen(
+    port,
+    console.log(`Server running on PORT ${port}...`)
+);
