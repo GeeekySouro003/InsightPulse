5f51ed598e187653219387fefc270cf70068ab0d
Souradip <dgsouro77@gmail.com>
2024-06-23 11:46:05 +0530

    updated the controller and fixed the routes

--- server/routes/updatePasswordRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authController = require("../controller/authController");
-
-router.put("/updatePassword", authController.updatePassword);
-
-module.exports = router;
